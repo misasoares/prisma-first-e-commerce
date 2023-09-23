47489fe4d4cc144a59fb7ffcfef264f290c4f4a6
misael soares <misasoaresms2@gmail.com>
2023-09-23 13:43:42 -0300

    criptografia de senha + acessos

--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -1,12 +1,30 @@
 import { Request, Response } from "express";
 import { prisma } from "../database/prisma";
+import { hash } from "bcryptjs";
 
 export const createUser = async (req: Request, res: Response) => {
-  const { name, email, password } = req.body;
+  const { name, email, password, accessName } = req.body;
+
+  const isUserUniqueEmail = await prisma.user.findUnique({
+    where: {
+      email
+    }
+  })
+
+  if(isUserUniqueEmail){
+    return res.status(400).json({message: "Já existe um usuário com este email."})
+  }
+
+  const hashPassword = await hash(password, 8)
 
   const user = await prisma.user.create({
-    data: { name, email, password },
+    data: { name, email, password: hashPassword, Access:{
+      connect:{
+        name: accessName
+      }
+    } },
   });
 
   return res.json(user);
 };
+
--- src/router.ts
+++ src/router.ts
@@ -1,8 +1,11 @@
 import { Router } from "express";
 
 import { createUser } from "./controller/UserController";
+import { createAccess, getAllAccesses } from "./controller/AccessController";
 
 
 export const router = Router()
 
 router.post('/user', createUser)
+router.post('/access', createAccess)
+router.get('/accesses', getAllAccesses)
